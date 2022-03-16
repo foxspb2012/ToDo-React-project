f69685ad01e21cd033a5394712ebc8b2a753062a
Евгений <foxspb2012@gmail.com>
2022-03-16 15:25:53 +0300

    Свойства компонентов передали в props через массив.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
 const App = () => {
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList/>
-        </div>
-    )
+
+  const todoData = [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a lunch', important: true}
+  ];
+
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList todos={todoData}/>
+    </div>
+  )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
